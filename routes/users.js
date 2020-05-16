e4ee633fd4f7fac29f1be4e50c49824230227e99
Aditya Jee <jeeadityajha1@gmail.com>
2020-05-16 06:13:57 +0530

    Added User Registration Feature

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,56 @@
 const express = require('express');
-
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route    POST api/users
 // @desc     Register a user
 // @access   Public
-router.post('/', (req, res) => {
-  res.send('Registers a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with six or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'Email already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Saved in DB');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the Keep Contact API' });
