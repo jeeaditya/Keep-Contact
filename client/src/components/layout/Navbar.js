f666b914b3bc0190d83e890f49c0b1425d15ad2b
Aditya Jee <jeeadityajha1@gmail.com>
2020-05-18 01:52:30 +0530

    Created Navbar, Home and About Links

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <Link to='/'>
+        <h1>
+          <i className={icon} />
+          &nbsp;&nbsp;
+          {title}
+        </h1>
+      </Link>
+
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.protoTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Keep Contact',
+  icon: 'fas fa-id-card-alt',
+};
+export default Navbar;
